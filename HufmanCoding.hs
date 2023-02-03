80fa9c8772bf59709b6561946e1c11a0af02765e
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2023-02-03 08:45:27 +0200

    Update HufmanCoding.hs

--- HufmanCoding.hs
+++ HufmanCoding.hs
@@ -81,13 +81,13 @@ makeTreeAndLeaves s = (makeTree (helperTree s ""), (tail (init (helperLeaves (in
         then reverse (')':res) 
         else helperTree cs (c:res)
     
-    helperLeaves "" = ""
+    helperLeaves "" = Just ""
     helperLeaves (c:cs) = 
       if c == ',' 
         then 
           (if cs == "" 
-          then error "Invalid string!" 
-          else reverse (helperTree (if (head cs) == ' ' then (tail cs) else cs) ""))
+          then Nothing 
+          else Just (reverse (helperTree (if (head cs) == ' ' then (tail cs) else cs) "")))
         else helperLeaves cs
 
 
